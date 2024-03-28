84ebf213005996bbcb21082cec07aa88bffb9043
GeeKaizer <georgemichaeldagogo@gmail.com>
2024-03-28 01:20:09 +0000

    following

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from newsapi import NewsApiClient
+from newsapi.newsapi_client import NewsApiClient
 import pandas as pd
 from newspaper import Article, Config
 import nltk
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import shutup; shutup.please()
 
 
 # Init
@@ -23,17 +24,16 @@ articles = top_headlines.get('articles', [])
     # Create a DataFrame with specific columns
 df = pd.DataFrame(articles, columns=['source','title','publishedAt','author','url'])
 
-
-
-
-
 # Data Cleaning
 df['source'] = df['source'].apply(lambda x: x['name'] if pd.notna(x) and 'name' in x else None)
+
+from newspaper import Article, Config
 
 def full_content(url):
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
     config = Config()
     config.browser_user_agent = user_agent
+    config.request_timeout = 10
     page = Article(url, config=config)
     
     try:
@@ -44,33 +44,42 @@ def full_content(url):
         print(f"Error retrieving content from {url}: {e}")
         return 'couldnt retrieve'
 
+
+
+
 df['content'] = df['url'].apply(full_content)
 df['content'] = df['content'].str.replace('\n', ' ')
 
 df = df[df['content'] != 'couldnt retrieve']
 
+
+
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+
 # Download the stopwords dataset
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
+
 # Function to count words without stopwords
 def count_words_without_stopwords(text):
-    # Check if the value is a string or bytes-like object
     if isinstance(text, (str, bytes)):
-        # Tokenize the text
         words = nltk.word_tokenize(str(text))
-        
-        # Remove stopwords
         stop_words = set(stopwords.words('english'))
         filtered_words = [word for word in words if word.lower() not in stop_words]
-        
-        # Return the count of non-stopwords
         return len(filtered_words)
     else:
-        # If the value is not a string or bytes-like, return 0
         return 0
-# Apply the function to the 'Text' column and create a new column 'WordCount'
+
+# Apply the function to the 'content' column using .loc for assignment
 df['WordCount'] = df['content'].apply(count_words_without_stopwords)
+
+
+
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Download the VADER lexicon
 nltk.download('vader_lexicon')
@@ -94,6 +103,5 @@ def get_sentiment(row):
     return sentiment, compound_score
 
 # Apply sentiment analysis to each row of the DataFrame
-df[['Sentiment', 'Compound_Score']] = df['content'].apply(lambda x: pd.Series(get_sentiment(x)))
-df
-
+df[['Sentiment', 'Compound_Score']] = df['content'].astype(str).apply(lambda x: pd.Series(get_sentiment(x)))
+print(df)
